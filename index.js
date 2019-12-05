7f77f74721338f1378d55eb9eaf4eb73accf61f3
boue <boue.germain@gmail.com>
2019-12-04 18:41:04 -0800

    By passing in bodyParser within app.use we are essentially ensuring that all of our route handlers use it instead of retyping it for every POST request

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/', (req, res) => {
     res.send(`
@@ -15,24 +20,8 @@ app.get('/', (req, res) => {
     `)
 })
 
-// middleware to parse form inputs
-const bodyParser = (req, res, next) => {
-    if (req.method === 'POST') {
-        req.on('data', () => {
-            const parsed = data.toString('utf8').split('&')
-            const formData = {}
-            for (let pair of parsed) {
-                const [key, value] = pair.split('=')
-                formData[key] = value
-            }
-            req.body = formData
-        })
-    } else {
-        next()
-    }
-}
-
-app.post('/', bodyParser, (req, res) => {
+app.post('/', (req, res) => {
+    console.log(req.body)
     res.send('Account Created!')
 })
 
