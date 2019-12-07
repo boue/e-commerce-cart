e66f250e7a04d46e91be39004b7e182d2947ee67
boue <boue.germain@gmail.com>
2019-12-06 17:50:57 -0800

    move write all to a helper function and check that username and password persist in json file

--- repositories/users.js
+++ repositories/users.js
@@ -19,11 +19,27 @@ class UsersRepository {
             encoding: 'utf8'
         }))
     }
+
+    async create(attrs) {
+        const records = await this.getAll()
+        records.push(attrs)
+
+        await this.writeAll(records)
+    }
+
+    async writeAll(records) {
+        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2))
+    }
 }
 
 const test = async () => {
 
     const repo = new UsersRepository('users.json')
+
+    await repo.create({
+        email: 'test@test.com',
+        password: 'password'
+    })
     const users = await repo.getAll();
     console.log(users)
 }
