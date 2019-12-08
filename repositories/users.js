51ae09a1f03872594a61c227cf7c5ec22c09ee14
boue <boue.germain@gmail.com>
2019-12-08 10:25:40 -0800

    Create compare passwords function as well as add password to record obj

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const crypto = require('crypto')
+const util = require(uti)
+
+const scrypt = util.promisify(crypto.scrypt)
 
 class UsersRepository {
     constructor(filename) {
@@ -23,8 +26,15 @@ class UsersRepository {
 
     async create(attrs) {
         attrs.id = this.randomId()
+        const salt = crypto.randomBytes(8).toString('hex')
+        const buf = await scrypt(attrs.password, salt, 64)
+
         const records = await this.getAll()
-        records.push(attrs)
+        const record = {
+            ...attrs,
+            password: `${buf.toString('hex')}.${salt}`
+        }
+        records.push(record)
 
         await this.writeAll(records)
 
@@ -33,6 +43,17 @@ class UsersRepository {
 
     async writeAll(records) {
         await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2))
+    }
+
+    async comparePasswords(saved, supplied) {
+        // const result = saved.split('.')
+        // const hashed = result[0]
+        // const salt = result[1]
+
+        const [hashed, salt] = saved.split('.')
+        const hashedSuppliedBuf = await scrypt(supplied, salt, 64)
+
+        return hashed === hashedSuppliedBuf.toString('hex')
     }
 
     randomId() {
