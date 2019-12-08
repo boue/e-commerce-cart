e85996120257463fb5652599b0e04a7e9f7b91b9
boue <boue.germain@gmail.com>
2019-12-08 15:42:42 -0800

    sanitize and normalize signup ize email and passwords in the post signup route

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const {
+    check,
+    validationResult
+} = require('express-validator')
 const usersRepo = require('../../repositories/users')
 const signupTemplate = require('../../views/admin/auth/signup')
 const signinTemplate = require('../../views/admin/auth/signin')
@@ -12,32 +16,44 @@ router.get('/signup', (req, res) => {
     }))
 })
 
-router.post('/signup', async (req, res) => {
-    const {
-        email,
-        password,
-        passwordConfirmation
-    } = req.body
+router.post('/signup',
+    [
+        check('email').trim().normalizeEmail().isEmail(),
+        check('password').trim().isLength({
+            min: 4,
+            max: 20
+        }),
+        check('passwordConfirmation').trim().isLength({
+            min: 4,
+            max: 20
+        })
+    ], async (req, res) => {
+        const errors = validationResult(req)
+        const {
+            email,
+            password,
+            passwordConfirmation
+        } = req.body
 
-    const existingUser = await usersRepo.getOneBy({
-        email: email
-    })
-    if (existingUser) {
-        return res.send('Email in use')
-    }
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match')
-    }
+        const existingUser = await usersRepo.getOneBy({
+            email: email
+        })
+        if (existingUser) {
+            return res.send('Email in use')
+        }
+        if (password !== passwordConfirmation) {
+            return res.send('Passwords must match')
+        }
 
-    // Create a user in our user repo to represent this person
-    const user = await usersRepo.create({
-        email,
-        password
+        // Create a user in our user repo to represent this person
+        const user = await usersRepo.create({
+            email,
+            password
+        })
+        // Store the id of that user inside of the users cookie
+        req.session.userId = user.id // Object ession is added by cookie session!
+        res.send('Account Created!')
     })
-    // Store the id of that user inside of the users cookie
-    req.session.userId = user.id // Object ession is added by cookie session!
-    res.send('Account Created!')
-})
 
 router.get('/signout', (req, res) => {
     req.session = null
