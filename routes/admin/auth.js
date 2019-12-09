cce66a91bebbf8b58c91d6fcddaf1c809bb602b6
boue <boue.germain@gmail.com>
2019-12-08 16:10:37 -0800

    No need for duplication logic here, using the middleware to make it cleaner and throw the errors in the console.log

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -18,7 +18,14 @@ router.get('/signup', (req, res) => {
 
 router.post('/signup',
     [
-        check('email').trim().normalizeEmail().isEmail(),
+        check('email').trim().normalizeEmail().isEmail().withMessage('Must be a valid email').custom(async (email) => {
+            const existingUser = await usersRepo.getOneBy({
+                email: email
+            })
+            if (existingUser) {
+                throw new Error('Email is in use')
+            }
+        }),
         check('password').trim().isLength({
             min: 4,
             max: 20
@@ -26,24 +33,21 @@ router.post('/signup',
         check('passwordConfirmation').trim().isLength({
             min: 4,
             max: 20
+        }).withMessage('Must be abetween 4 and 20 characters').custom((passwordConfirmation, {
+            req
+        }) => {
+            if (passwordConfirmation !== req.body.password) {
+                throw new Error('Passwords must match')
+            }
         })
     ], async (req, res) => {
         const errors = validationResult(req)
+        console.log(errors)
         const {
             email,
             password,
             passwordConfirmation
         } = req.body
-
-        const existingUser = await usersRepo.getOneBy({
-            email: email
-        })
-        if (existingUser) {
-            return res.send('Email in use')
-        }
-        if (password !== passwordConfirmation) {
-            return res.send('Passwords must match')
-        }
 
         // Create a user in our user repo to represent this person
         const user = await usersRepo.create({
